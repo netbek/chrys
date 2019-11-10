2905000d03d3f12d237ee407a594ce2318ec4fcd
Hein Bekker <hein@siyavula.com>
2019-11-10 08:00:32 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chrys",
-    version="2.0.25",
+    version="2.0.26",
     author="Hein Bekker",
     author_email="hein@netbek.co.za",
     description="A collection of color palettes for mapping and visualisation",
