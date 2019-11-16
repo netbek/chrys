c25015b55528c2511cd39bdd7c75e6955e0c1b99
Hein Bekker <hein@siyavula.com>
2019-11-16 18:48:37 +0200

    Change entry point to UMD

--- index.js
+++
@@ -1,8 +0,0 @@
-var palettes = require('./data/palettes');
-
-module.exports = {
-  palette: function(name, size) {
-    return palettes[name].sizes[size || palettes[name].defaultSize];
-  },
-  palettes: palettes
-};
