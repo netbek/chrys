19bf87c039187b8c68b4698729b278dc7b07e364
Hein Bekker <hein@siyavula.com>
2019-11-24 18:07:21 +0200

    Fix test data

--- tests/py/utils_tests.py
+++ tests/py/utils_tests.py
@@ -6,4 +6,4 @@ from chrys.utils import best_color_contrast
 class BestColorContrastTests(unittest.TestCase):
 
     def test_black_on_red(self):
-        self.assertEqual(best_color_contrast('#f00000', ['#000000', '#ffffff']), '#000000')
+        self.assertEqual(best_color_contrast('#ff0000', ['#000000', '#ffffff']), '#000000')
