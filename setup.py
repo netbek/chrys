79849df84457e9db34a1500a9aacfaf1341a1050
Hein Bekker <hein@siyavula.com>
2019-11-28 10:35:44 +0200

    Fix inconsistent quotes

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="chrys",
-    version="3.1.0",
-    author="Hein Bekker",
-    author_email="hein@netbek.co.za",
-    description="A collection of color palettes for mapping and visualisation",
+    name='chrys',
+    version='3.1.0',
+    author='Hein Bekker',
+    author_email='hein@netbek.co.za',
+    description='A collection of color palettes for mapping and visualisation',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     license='BSD-3-Clause',
-    url="https://github.com/netbek/chrys",
+    url='https://github.com/netbek/chrys',
     install_requires=[
         'matplotlib == 2.2.4, == 2.*',
         'nose >= 1.3.7, == 1.*',
@@ -22,9 +22,9 @@ setuptools.setup(
     python_requires='>=2.7',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: POSIX :: Linux",
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
     ],
 )
