5e7e53822e7c9312214071f2c3889b025ada4ee6
Hein Bekker <hein@siyavula.com>
2021-11-02 23:42:21 +0200

    Fix postinstall

--- scripts/npm/postinstall.sh
+++ scripts/npm/postinstall.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
 # Disable imported modules that currently do not work with Node 14 nor the `esm` package (`node -r esm`)
-sed -i 's/^import/\/\/ import/g' node_modules/@bokeh/bokehjs/build/js/lib/api/palettes.js
+# We check whether the file exists because `@bokeh/bokehjs` might not be installed by the parent package.
+FILE='node_modules/@bokeh/bokehjs/build/js/lib/api/palettes.js'
+if [ -f "$FILE" ]; then
+  sed -i 's/^import/\/\/ import/g' "$FILE"
+fi
